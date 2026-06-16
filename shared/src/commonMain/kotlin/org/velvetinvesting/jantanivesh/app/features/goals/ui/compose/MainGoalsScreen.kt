d55109b80a83c6628847b9491a0df4784a181cb2
YashGitsUp <yogendra@pantharinfohub.com>
2026-06-16 20:20:15 +0530

    Added Goals feature with partial navigational setup

--- shared/src/commonMain/kotlin/org/velvetinvesting/jantanivesh/app/features/goals/ui/compose/MainGoalsScreen.kt
+++ shared/src/commonMain/kotlin/org/velvetinvesting/jantanivesh/app/features/goals/ui/compose/MainGoalsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
@@ -18,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jantanivesh.shared.generated.resources.Res
 import jantanivesh.shared.generated.resources.goals_splash
 import org.jetbrains.compose.resources.painterResource
@@ -27,45 +29,73 @@ import org.velvetinvesting.jantanivesh.app.features.core.ui.composables.AppButto
 import org.velvetinvesting.jantanivesh.app.features.core.ui.composables.AppButtonDefaults
 import org.velvetinvesting.jantanivesh.app.features.core.ui.composables.BackHeader
 import org.velvetinvesting.jantanivesh.app.features.core.ui.modifierextensions.genericDropShadow
+import org.velvetinvesting.jantanivesh.app.features.goals.ui.viewmodels.MainGoalsEvent
 
 @Preview(showBackground = true)
 @Composable
 fun MainGoalsScreenPreview() {
     JantaNiveshTheme {
-        MainGoalsScreen()
+        MainGoalsScreen(
+            pv = PaddingValues(0.dp),
+            onBackClick = {},
+            handleEvent = {}
+        )
     }
 }
 
 @Composable
-fun MainGoalsScreen(modifier: Modifier = Modifier) {
+fun MainGoalsScreen(
+    pv: PaddingValues,
+    onBackClick: () -> Unit,
+    handleEvent: (MainGoalsEvent) -> Unit,
+    modifier: Modifier = Modifier
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize().padding(horizontal = Spacing.dp16)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(pv)
+            .padding(horizontal = Spacing.dp16)
     ) {
-        BackHeader(onBack = { TODO() }, title = "Your Goals", modifier = Modifier.statusBarsPadding())
+        BackHeader(
+            onBack = onBackClick,
+            title = "Your Goals",
+            modifier = Modifier.statusBarsPadding()
+        )
+
         Box(
             contentAlignment = Alignment.Center,
-            modifier = Modifier.weight(1f).padding(horizontal = Spacing.dp16)
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .padding(horizontal = Spacing.dp16)
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.spacedBy(Spacing.dp48),
-                ) {
+            ) {
                 Image(
                     painter = painterResource(Res.drawable.goals_splash),
                     contentDescription = null,
-                    modifier = Modifier.size(
-                        Spacing.dp285
-                    )
+                    modifier = Modifier.size(Spacing.dp285)
                 )
                 Text(
-                    "Set clear financial goals and take small steps each month to achieve them!",
+                    text = "Set clear financial goals and take small steps each month to achieve them!",
                     style = MaterialTheme.typography.labelSmall,
                     textAlign = TextAlign.Center
                 )
             }
         }
-        AppButton(text = "Set a Goal", onClick = { /*TODO*/ }, modifier = Modifier.genericDropShadow().fillMaxWidth().navigationBarsPadding() , style = AppButtonDefaults.style(shape = RoundedCornerShape(Spacing.dp16)))
+
+        AppButton(
+            text = "Set a Goal",
+            onClick = { handleEvent(MainGoalsEvent.OnSetGoalClicked) },
+            modifier = Modifier
+                .genericDropShadow()
+                .fillMaxWidth()
+                .navigationBarsPadding(),
+            style = AppButtonDefaults.style(shape = RoundedCornerShape(Spacing.dp16))
+        )
     }
 }
